2e21c64492f6707a793c346ec18df0445482365c
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2021-05-30 15:05:32 -0400

    initial commit

---
+++ build.bat
@@ -0,0 +1,5 @@
+cd src/
+rgbasm -o ../build/main.o main.asm
+cd ../build
+rgblink -o main.gb main.o
+rgbfix -v -p 0 main.gb
---
+++ src/main.asm
@@ -0,0 +1,39 @@
+; GB Main bank - entry point
+
+INCLUDE "lib/gbhw.inc" ; standard hardware definitions from devrs.com
+
+; IRQs
+SECTION "Vblank", ROM0[$0040]
+  reti
+SECTION "LCDC", ROM0[$0048]
+  reti
+SECTION "Timer_Overflow", ROM0[$0050]
+  reti
+SECTION "Serial", ROM0[$0058]
+  reti
+SECTION "p1thru4", ROM0[$0060]
+  reti
+
+; ****************************************************************************************
+; boot loader jumps to here.
+; ****************************************************************************************
+SECTION "start", ROM0[$0100]
+nop
+jp begin
+
+; ****************************************************************************************
+; ROM HEADER
+; ****************************************************************************************
+; ROM header
+ ROM_HEADER ROM_NOMBC, ROM_SIZE_32KBYTE, RAM_SIZE_0KBYTE
+
+; ****************************************************************************************
+; Main code Start
+; ****************************************************************************************
+begin:
+  nop
+  di
+.wait
+  halt
+  nop
+  jr .wait
